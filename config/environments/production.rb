d2f31c1eaa77f238f8196db9bcb3d2f00bc92f02
igatai <taiti0614@gmail.com>
2020-03-12 14:01:52 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,14 +21,14 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :scss
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # config.assets.compile = false
   config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  # config.serve_static_assets = true
-  config.assets.compile = true
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'

26e230f4b52af8143602918db34a6634a589619f
igatai <taiti0614@gmail.com>
2020-03-12 11:43:37 +0900

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  config.serve_static_assets = true
+  # config.serve_static_assets = true
   config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.

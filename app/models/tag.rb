1d4797a3d4c04713083a918f45268620cdd51b32
igatai <taiti0614@gmail.com>
2020-03-28 23:25:04 +0900

    commit

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,5 @@ class Tag < ApplicationRecord
   has_many :tag_memos
   has_many :memos, through: :tag_memos, dependent: :destroy
   belongs_to :user
+  belongs_to :folder
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "memos#index"
   resources :users, only: [:edit, :update]
   resources :memos, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :folders,  only:[:index, :show, :new, :create, :edit, :update, :destroy]
   resources :tags,  only:[:index, :show, :new, :create, :edit, :update, :destroy] do
     collection do
       get 'search'

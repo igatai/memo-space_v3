5e564e1c7085b549886967e8318175282187ce9e
igatai <taiti0614@gmail.com>
2020-04-04 11:58:40 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   root "memos#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:new, :edit, :update]
   resources :memos, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :folders,  only:[:index, :show, :new, :create, :edit, :update, :destroy]
   resources :tags,  only:[:index, :show, :new, :create, :edit, :update, :destroy] do
